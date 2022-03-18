4c00c5789027dcbed4451e3963666621bf0cb065
Luca Candussi <candussi@student.tugraz.at>
2022-03-18 21:21:59 +0100

    Initial Commit

---
+++ 20220318_tryCPU/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  20220318_tryCPU
+//
+//  Created by Luca Candussi on 18.03.22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+  return 0;
+}
